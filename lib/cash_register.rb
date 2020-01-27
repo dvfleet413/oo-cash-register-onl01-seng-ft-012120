e2d812eb5b946cb787b92fb19acfe9ea39b0e086
David Van Fleet <dvfleet413@gmail.com>
2020-01-27 18:02:02 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
-
+require 'pry'
+class CashRegister
+  attr_accessor :total, :previous_total, :discount, :items
+  def initialize(employee_discount = 0)
+    @items = []
+    @total = 0
+    @discount = employee_discount
+  end
+  
+  def add_item(name, price, quantity = 1)
+    @previous_total = @total
+    @total += price * quantity
+    quantity.times {@items << name}
+  end
+  
+  def apply_discount
+    discount_decimal = (100-@discount.to_f)/100
+    message = nil
+    if @discount > 0
+      @total *= discount_decimal
+      message =  "After the discount, the total comes to $#{@total.round}."
+    else
+      message = "There is no discount to apply."
+    end
+  message
+  end 
+  
+  def void_last_transaction
+    @total = @previous_total
+  end
+end 
