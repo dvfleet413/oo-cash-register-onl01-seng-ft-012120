5dd7e9c21f514c7d4b43a09cffdbc52f10819111
David Van Fleet <dvfleet413@gmail.com>
2020-01-27 17:44:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-  attr_accessor :total, :previous_total :discount, :items
+  attr_accessor :total, :previous_total, :discount, :items
   def initialize(employee_discount = 0)
     @total = 0
     @discount = employee_discount
